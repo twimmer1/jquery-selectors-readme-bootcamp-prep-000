de9fdd441fd7609e96067ff3c11be656a873c6b9
Tristan Wimmer <tristan.a.wimmer@gmail.com>
2018-04-08 19:20:45 +0000

    automatically backed up by learn

--- js/selectors.js
+++ js/selectors.js
@@ -1,1 +1,20 @@
-// declare your functions here...
+function paragraphSelector() {
+  var elements = $('p');
+  return elements
+}
+
+function lastImageSelector() {
+  return $('img:last')
+}
+
+function ninjaBabySelector() {
+  return $('#baby-ninja')
+}
+
+function divSelector() {
+  return $('.pics')
+}
+
+function firstListItem() {
+  $('parent-tag child-tag:first-child')
+}
